c2318bf56c481d5d6e51b7b960022e6ad2174e37
최영훈 <c_yh0327@naver.com>
2020-10-09 17:52:14 +0900

    1차세미나 진짜마지막

--- 1st-seminar/teamMaker.js
+++ 1st-seminar/teamMaker.js
@@ -1,11 +1,11 @@
 const member = require('./partMember');
 
-const noShowMembers = [];
+const noShowMembers = ["오승재", "김기문", "박경선", "이주은", "이현종", "김현기"];
 const offlineMembers = member
 .filter(it => !noShowMembers.includes(it.name))
 .sort((a, b) => Math.random() - Math.random())
 .sort((a, b) => a.status.localeCompare(b.status));
-const team = {team1:[],team2:[],team3:[],team4:[],team5:[],team6:[]};
+const team = {team1:[], team2:[], team3:[], team4:[], team5:[], team6:[]};
 
 offlineMembers.forEach((member, index) => {
   team[`team${(index%6)+1}`].push(member);
