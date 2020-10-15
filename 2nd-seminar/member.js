f7c8f9a08f11a5d97c15af722115c3ad5aa37876
최영훈 <c_yh0327@naver.com>
2020-10-16 00:18:16 +0900

    promise quiz

---
+++ 2nd-seminar/member.js
@@ -0,0 +1,224 @@
+module.exports = [
+  {
+      "name": "최정재",
+      "part": "Server",
+      "status": "OB",
+      "gender": "남"
+  },
+  {
+      "name": "박남선",
+      "part": "iOS",
+      "status": "OB",
+      "gender": "여"
+  },
+  {
+      "name": "신민상",
+      "part": "Server",
+      "status": "OB",
+      "gender": "남"
+  },
+  {
+      "name": "강효원",
+      "part": "Server",
+      "status": "OB",
+      "gender": "여"
+  },
+  {
+      "name": "류세훈",
+      "part": "Server",
+      "status": "OB",
+      "gender": "여"
+  },
+  {
+      "name": "이가영",
+      "part": "Server",
+      "status": "OB",
+      "gender": "여"
+  },
+  {
+      "name": "이영현",
+      "part": "Server",
+      "status": "OB",
+      "gender": "여"
+  },
+  {
+      "name": "남궁찬",
+      "part": "Server",
+      "status": "OB",
+      "gender": "남"
+  },
+  {
+      "name": "김민주",
+      "part": "Server",
+      "status": "OB",
+      "gender": "여"
+  },
+  {
+      "name": "최정훈",
+      "part": "iOS",
+      "status": "OB",
+      "gender": "여"
+  },
+  {
+      "name": "박주은",
+      "part": "Server",
+      "status": "OB",
+      "gender": "여"
+  },
+  {
+      "name": "이현준",
+      "part": "Server",
+      "status": "OB",
+      "gender": "여"
+  },
+  {
+      "name": "김수민",
+      "part": "Server",
+      "status": "OB",
+      "gender": "여"
+  },
+  {
+      "name": "김현상",
+      "part": "Server",
+      "status": "OB",
+      "gender": "여"
+  },
+  {
+      "name": "김채림",
+      "part": "Server",
+      "status": "OB",
+      "gender": "여"
+  },
+  {
+      "name": "이현상",
+      "part": "Server",
+      "status": "YB",
+      "gender": "남"
+  },
+  {
+      "name": "홍혜진",
+      "part": "Server",
+      "status": "YB",
+      "gender": "여"
+  },
+  {
+      "name": "오승준",
+      "part": "iOS",
+      "status": "YB",
+      "gender": "남"
+  },
+  {
+      "name": "양재욱",
+      "part": "Server",
+      "status": "YB",
+      "gender": "남"
+  },
+  {
+      "name": "최선준",
+      "part": "Server",
+      "status": "YB",
+      "gender": "남"
+  },
+  {
+      "name": "박상수",
+      "part": "Server",
+      "status": "YB",
+      "gender": "남"
+  },
+  {
+      "name": "임찬기",
+      "part": "iOS",
+      "status": "YB",
+      "gender": "남"
+  },
+  {
+      "name": "박진호",
+      "part": "Server",
+      "status": "YB",
+      "gender": "남"
+  },
+  {
+      "name": "신지한",
+      "part": "iOS",
+      "status": "YB",
+      "gender": "여"
+  },
+  {
+      "name": "김영은",
+      "part": "Server",
+      "status": "YB",
+      "gender": "여"
+  },
+  {
+    "name": "최영훈",
+    "part": "iOS",
+    "status": "YB",
+    "gender": "여"
+  },
+  {
+      "name": "송정훈",
+      "part": "iOS",
+      "status": "YB",
+      "gender": "남"
+  },
+  {
+      "name": "강준수",
+      "part": "Server",
+      "status": "YB",
+      "gender": "남"
+  },
+  {
+      "name": "김중현",
+      "part": "Server",
+      "status": "YB",
+      "gender": "여"
+  },
+  {
+      "name": "김기찬",
+      "part": "Server",
+      "status": "YB",
+      "gender": "남"
+  },
+  {
+      "name": "김우영",
+      "part": "Server",
+      "status": "YB",
+      "gender": "남"
+  },
+  {
+      "name": "윤가인",
+      "part": "Server",
+      "status": "YB",
+      "gender": "여"
+  },
+  {
+      "name": "이예진",
+      "part": "Server",
+      "status": "YB",
+      "gender": "여"
+  },
+  {
+      "name": "박수진",
+      "part": "Server",
+      "status": "YB",
+      "gender": "여"
+  },
+  {
+      "name": "최다슬",
+      "part": "Server",
+      "status": "YB",
+      "gender": "여"
+  },
+  {
+      "name": "한승아",
+      "part": "Server",
+      "status": "YB",
+      "gender": "여"
+  },
+  {
+      "name": "김재은",
+      "part": "Server",
+      "status": "YB",
+      "gender": "여"
+  }
+]
---
+++ 2nd-seminar/quiz.js
@@ -0,0 +1,24 @@
+const members = require('./member');
+
+function getFemale(members) {
+  return new Promise(function (resolve, reject) {
+  /** logic */
+  })
+}
+
+function getYB(members) {
+  return new Promise(function (resolve, reject) {
+  /** logic */
+  })
+}
+
+function getiOS(members) {
+  return new Promise(function (resolve, reject) {
+  /** logic */
+  })
+}
+
+getFemale() //
+  .then()
+  .then()
+  .then();
