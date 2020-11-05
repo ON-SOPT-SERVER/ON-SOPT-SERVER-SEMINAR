4cf79293a78fcfcb4ad0a06086a372a29bc88222
최영훈 <c_yh0327@naver.com>
2020-11-06 03:00:09 +0900

    [추가] 4차 세미나 실습

---
+++ 4th-seminar/models/index.js
@@ -0,0 +1,17 @@
+const Sequelize = require('sequelize');
+const env = process.env.NODE_ENV || 'development';
+const config = require('../config/config.json')[env];
+const db = {};
+
+let sequelize;
+if (config.use_env_variable) {
+  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+} else {
+  sequelize = new Sequelize(config.database, config.username, config.password, config);
+}
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+db.User = require('./user')(sequelize, Sequelize);
+
+module.exports = db;
