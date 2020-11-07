9f89a6da2b16f85b457c39676bc7934bb4bd1c57
최영훈 <c_yh0327@naver.com>
2020-11-07 12:11:59 +0900

    [수정] 최신화

--- 4th-seminar/app.js
+++ 4th-seminar/app.js
@@ -5,7 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const { sequelize } = require('./models');
 
-sequelize.sync({ force: false })
+sequelize.sync({ alter: true })
 .then(() => {
   console.log('데이터베이스 연결 성공.');
 })
